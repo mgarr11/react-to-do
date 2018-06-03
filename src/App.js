62799b5c6ffed50847ced5c956629df17947327f
Matt Garrett <magarrett11@gmail.com>
2018-06-03 08:13:59 -0600

    6.3.18 re-try

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ToDo from './components/ToDo.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <ul>
+      <ToDo />
+      <ToDo />
+      </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
